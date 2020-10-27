4405a526afa548a2b8fc4379c2aa0a12f37bb9f8
Renan Nakazawa <renan.nakazawa0@gmail.com>
2020-10-27 02:34:46 -0300

    reduce keep alive interval

--- server/websocket.js
+++ server/websocket.js
@@ -2,7 +2,7 @@ const WebSocketServer = require("websocket").server;
 const { getRandomQuote } = require("./quotes")
 
 module.exports = (app) => {
-  wsServer = new WebSocketServer({ httpServer: app });
+  wsServer = new WebSocketServer({ httpServer: app, keepaliveInterval: 5000, keepaliveGracePeriod: 2000 });
 
   wsServer.on("request", (request) => {
     const connection = request.accept("echo-protocol", request.origin);
@@ -12,6 +12,10 @@ module.exports = (app) => {
         const data = JSON.parse(message.utf8Data)
         if (data.type == "gameStart") {
           data.quote = getRandomQuote()
+          wsServer.unmount()
+        }
+        if (data.type == "finished") {
+          wsServer.mount({ httpServer: app })
         }
         wsServer.broadcast(JSON.stringify(data));
       } catch (err) {
